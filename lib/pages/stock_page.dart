9261e96f9c0feb22459c06df963c0661973ccc52
adrianeka <78060253+adrianeka@users.noreply.github.com>
2024-03-24 13:02:20 +0700

    add: stock page

--- lib/pages/stock_page.dart
+++ lib/pages/stock_page.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
 import '../colors.dart';
 
-class StockPage extends StatelessWidget {
+class StockPage extends StatefulWidget {
+  @override
+  _StockPageState createState() => _StockPageState();
+}
+
+class _StockPageState extends State<StockPage> {
   var height, width;
+
+  List imgData = [
+    "assets/icon/stock.png",
+    "assets/icon/stock.png",
+    "assets/icon/stock.png",
+    "assets/icon/stock.png",
+  ];
+  List itemName = [
+    "Accu",
+    "Oli",
+    "Radiator",
+    "Ban",
+  ];
+
+  List itemPrice = [
+    "20.000",
+    "30.000",
+    "499.999",
+    "200.000",
+  ];
+
+  List itemStock = [
+    "10",
+    "20",
+    "30",
+    "40",
+  ];
+
+  String selectedFilter = 'Semua';
+  String selectedSort = 'A-Z';
 
   @override
   Widget build(BuildContext context) {
     height = MediaQuery.of(context).size.height;
     width = MediaQuery.of(context).size.width;
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Stock Gudang'),
@@ -21,12 +55,146 @@ class StockPage extends StatelessWidget {
                 );
               },
               icon: const Icon(Icons.search)),
+          IconButton(
+              onPressed: () async {
+                // await showFilterSortDialog(context);
+              },
+              icon: const Icon(Icons.filter_list)),
         ],
         backgroundColor: primary,
         foregroundColor: Colors.white,
       ),
-      body: PageView(
-        children: [],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  DropdownButton<String>(
+                    value: selectedFilter,
+                    onChanged: (value) {
+                      setState(() {
+                        selectedFilter = value!;
+                      });
+                    },
+                    items: ['Semua', 'Filter 1', 'Filter 2', 'Filter 3']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  DropdownButton<String>(
+                    value: selectedSort,
+                    onChanged: (value) {
+                      setState(() {
+                        selectedSort = value!;
+                      });
+                    },
+                    items: ['A-Z', 'Z-A']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 8), // Tambahkan jarak antara dropdown dan konten
+            Container(
+              height: height,
+              width: width,
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 1,
+                  childAspectRatio: 3.1,
+                  mainAxisSpacing: 1,
+                  crossAxisSpacing: 1,
+                ),
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: imgData.length,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    child: Container(
+                      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black26,
+                            spreadRadius: 1,
+                            blurRadius: 2,
+                          ),
+                        ],
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.all(5),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Image.asset(
+                              imgData[index],
+                              width: 70,
+                            ),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.only(top: 8),
+                                    child: Text(
+                                      itemName[index],
+                                      style: TextStyle(
+                                        fontSize: 25,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(top: 2),
+                                    child: Text(
+                                      'Rp. ${itemPrice[index]}',
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(top: 4),
+                                    child: Text(
+                                      'Stok : ${itemStock[index]}',
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -38,6 +206,7 @@ class StockSearch extends SearchDelegate {
     'Oli',
     'Radiator',
   ];
+
   @override
   List<Widget> buildActions(BuildContext context) {
     return [
@@ -45,7 +214,7 @@ class StockSearch extends SearchDelegate {
           onPressed: () {
             query = '';
           },
-          icon: const Icon(Icons.clear))
+          icon: const Icon(Icons.clear)),
     ];
   }
 
