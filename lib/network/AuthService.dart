7cb4a3c3e33bce779a0a510d1e672b997fd770f5
adrianeka <78060253+adrianeka@users.noreply.github.com>
2024-05-25 19:43:44 +0700

    feat: integration api register

--- lib/network/AuthService.dart
+++ lib/network/AuthService.dart
@@ -1,16 +1,15 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import '../utils.dart';
-import '../models/Auth.dart';
+// import '../models/Auth.dart';
 
 class AuthService {
   static Future<Map<String, dynamic>> login(
       String email, String password) async {
     try {
       final response = await http.post(
-        Uri.parse(
-            url + '/api/login'), // Ganti dengan URL endpoint login API kamu
+        Uri.parse(url + '/api/login'),
         body: {
           'email': email,
           'password': password,
@@ -28,6 +27,41 @@ class AuthService {
     } catch (e) {
       print('Error logging in: $e');
       throw Exception('Failed to login');
+    }
+  }
+
+  static Future<Map<String, dynamic>> register(String name, String username,
+      String email, String password, String confirmPassword) async {
+    try {
+      final response = await http.post(
+        Uri.parse(url + '/api/register'),
+        body: {
+          'name': name,
+          'email': email,
+          'username': username,
+          'password': password,
+          'password_confirmation': confirmPassword,
+        },
+      );
+
+      print('Register Response: ${response.body}');
+
+      Map<String, dynamic> data = json.decode(response.body);
+
+      if (response.statusCode == 200) {
+        if (data.containsKey('response') && data['response'] == 422) {
+          throw Exception(data['message']);
+        } else if (data.containsKey('success') && data['success'] == true) {
+          return data;
+        } else {
+          throw Exception('Unknown error occurred');
+        }
+      } else {
+        throw Exception(data['message'] ?? 'Failed to register');
+      }
+    } catch (e) {
+      print('Error logging in: $e');
+      throw Exception('Failed to register');
     }
   }
 }
--- lib/widgets/register_form.dart
+++ lib/widgets/register_form.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:quickalert/quickalert.dart';
 import 'package:stocklab_fe/colors.dart';
+import 'package:stocklab_fe/network/AuthService.dart';
 
-class RegisterForm extends StatelessWidget {
+class RegisterForm extends StatefulWidget {
   final double height;
   final double width;
 
@@ -9,6 +11,38 @@ class RegisterForm extends StatelessWidget {
     required this.height,
     required this.width,
   });
+
+  @override
+  _RegisterFormState createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<RegisterForm> {
+  late TextEditingController nameController;
+  late TextEditingController usernameController;
+  late TextEditingController emailController;
+  late TextEditingController passwordController;
+  late TextEditingController confirmPasswordController;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    nameController = TextEditingController();
+    usernameController = TextEditingController();
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+    confirmPasswordController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    usernameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,8 +60,8 @@ class RegisterForm extends StatelessWidget {
                   padding: EdgeInsets.only(bottom: 20.0, top: 50.0),
                   child: Image.asset(
                     'assets/icon/icon.png',
-                    height: height / 7,
-                    width: height / 7,
+                    height: widget.height / 7,
+                    width: widget.height / 7,
                   ),
                 ),
                 Padding(
@@ -44,7 +78,7 @@ class RegisterForm extends StatelessWidget {
             ),
           ),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.9),
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
             child: Text(
               'Daftar Baru',
               style: TextStyle(
@@ -60,6 +94,49 @@ class RegisterForm extends StatelessWidget {
               style: TextStyle(
                 color: Colors.white,
               ),
+              controller: nameController,
+              decoration: InputDecoration(
+                labelText: 'Nama Lengkap',
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                ),
+                labelStyle: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: TextField(
+              style: TextStyle(
+                color: Colors.white,
+              ),
+              controller: usernameController,
+              decoration: InputDecoration(
+                labelText: 'Username',
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                ),
+                labelStyle: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: TextField(
+              style: TextStyle(
+                color: Colors.white,
+              ),
+              controller: emailController,
               decoration: InputDecoration(
                 labelText: 'Email',
                 enabledBorder: UnderlineInputBorder(
@@ -77,6 +154,7 @@ class RegisterForm extends StatelessWidget {
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.0),
             child: TextField(
+              controller: passwordController,
               decoration: InputDecoration(
                 labelText: 'Password',
                 enabledBorder: UnderlineInputBorder(
@@ -95,6 +173,7 @@ class RegisterForm extends StatelessWidget {
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.0),
             child: TextField(
+              controller: confirmPasswordController,
               decoration: InputDecoration(
                 labelText: 'Confirm Password',
                 enabledBorder: UnderlineInputBorder(
@@ -115,12 +194,57 @@ class RegisterForm extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 20.0),
             child: ElevatedButton(
               onPressed: () async {
-                // Registration logic here
+                setState(() {
+                  isLoading = true;
+                });
+                try {
+                  await AuthService.register(
+                    nameController.text,
+                    usernameController.text,
+                    emailController.text,
+                    passwordController.text,
+                    confirmPasswordController.text,
+                  );
+                  QuickAlert.show(
+                    context: context,
+                    type: QuickAlertType.success,
+                    text: "Registration successful.",
+                  );
+                } catch (e) {
+                  // QuickAlert.show(
+                  //   context: context,
+                  //   type: QuickAlertType.error,
+                  //   text: e.toString(),
+                  // );
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text('Registrasi Gagal'),
+                        content: Text(e.toString()),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text('OK'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                } finally {
+                  setState(() {
+                    isLoading = false;
+                  });
+                }
               },
-              child: Text(
-                'Register',
-                style: TextStyle(color: primary, fontSize: 18),
-              ),
+              child: isLoading
+                  ? CircularProgressIndicator() // Tampilkan indicator loading jika sedang loading
+                  : Text(
+                      'Register',
+                      style: TextStyle(color: primary, fontSize: 18),
+                    ),
               style: ButtonStyle(
                 backgroundColor: MaterialStateProperty.all<Color>(secondary),
                 padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
